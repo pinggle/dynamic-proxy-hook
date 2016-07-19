8ef35d23a3d6bd4bce6aa217204efdeb85cb5332
yanping <ypist@qq.com>
2016-07-19 15:56:07 +0800

    1.整理代码;

--- src/main/java/com/weishu/upf/dynamic_proxy_hook/app2/hook/HookHelper.java
+++ src/main/java/com/weishu/upf/dynamic_proxy_hook/app2/hook/HookHelper.java
@@ -9,6 +9,24 @@ import com.weishu.upf.dynamic_proxy_hook.app2.MainActivity;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
+/***********************************************************************
+ Android23下 ContextImpl::startActivity 函数定义为:
+ -----------------------------------------------------------------------
+ public void ContextImpl::startActivity(Intent intent, Bundle options) {
+    warnIfCallingFromSystemProcess();
+    if ((intent.getFlags()&Intent.FLAG_ACTIVITY_NEW_TASK) == 0) {
+        throw new AndroidRuntimeException(
+        "Calling startActivity() from outside of an Activity "
+        + " context requires the FLAG_ACTIVITY_NEW_TASK flag."
+        + " Is this really what you want?");
+    }
+    mMainThread.getInstrumentation().execStartActivity(
+    getOuterContext(), mMainThread.getApplicationThread(), null,
+        (Activity) null, intent, -1, options);
+ }
+ -----------------------------------------------------------------------
+ ***********************************************************************/
+
 /**
  * @author weishu
  * @date 16/1/28
@@ -39,12 +57,21 @@ public class HookHelper {
     }
 
     public static void attachActivity(MainActivity activity) throws Exception {
+
+        // 传递当前的 Activity 对象进来;
+
+        // 拿到原始的 mInstrumentation字段
         Class<?> activityClass = Activity.class;
         Field mInstrumentation = activityClass.getDeclaredField("mInstrumentation");
         mInstrumentation.setAccessible(true);
         Instrumentation base = (Instrumentation) mInstrumentation.get(activity);
+
+        // 创建代理对象
         AppInstrumentation appInstrumentation = new AppInstrumentation(base);
+
+        // 偷梁换柱
         mInstrumentation.set(activity, appInstrumentation);
+
         Log.v(TAG, "DTPrint HOOK (Activity::startActivity) Over");
     }
 }
